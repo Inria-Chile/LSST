c845e6a00c427ac7982bed7f65c536621c12f4eb
spereirag <sebastian.pereira@inria.cl>
2017-08-22 16:39:43 -0300

    Server returns json. App reads data from server.

--- LSST-app/src/components/Survey/Survey.js
+++ LSST-app/src/components/Survey/Survey.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 // import Skymap from '../Skymap/Skymap';
 import MainSkymap from '../Skymap/MainSkymap';
 import MiniSkymaps from '../Skymap/MiniSkymaps';
-// import Charts from '../Charts/Charts';
+import Charts from '../Charts/Charts';
 import Sidebar from '../Sidebar/Sidebar';
 import SurveyControls from '../SurveyControls/SurveyControls';
 import './Survey.css';
@@ -12,53 +12,83 @@ class Survey extends Component {
     constructor(props) {
         super(props);
         this.mainSkymap = null;
-        this.children = [];
+        this.miniSkymap = null;
     }
 
     drawFrame = (timestamp) => {
         // console.log('Parent Drawframe')
         this.mainSkymap.drawFrame(timestamp);
-        this.children.forEach(function (child) {
-            child.drawFrame(timestamp);
-        });
+        this.miniSkymap.drawFrame(timestamp);
         requestAnimationFrame(this.drawFrame);
     }
-
-    componentDidMount() {
-        // this.cel = this.mainSkymap.getCelestial();
-        // requestAnimationFrame(this.drawFrame);
-    }
-
+    
     setEcliptic = (show) => {
         this.mainSkymap.setEcliptic(show);
     }
-
+    
     setGalactic = (show) => {
         this.mainSkymap.setGalactic(show);
     }
-
+    
     setMoon = (show) => {
-        this.mainSkymap.setMoon(show);        
+        this.mainSkymap.setMoon(show);
+        fetch('survey/playback/observationsCount', {
+            
+        });
     }
-
+    
     setTelescopeRange = (show) => {
-        this.mainSkymap.setTelescopeRange(show);        
+        this.mainSkymap.setTelescopeRange(show);
     }
-
+    
     setSidebar = (show) => {
         this.sidebar.setState({
             sidebarOpen: show
         })
     }
-
+    
     setProjection = (proj) => {
         this.mainSkymap.setProjection(proj);
     }
-
+    
     setDisplayedFilter = (filter) => {
         this.mainSkymap.setDisplayedFilter(filter);
     }
+    
+    setDataByDate = (startDate, endDate, cb) => {
+        return fetch(`survey/playback/observationsCount?start_date=${startDate}&end_date=${endDate}`, {
+            accept: "application/json"
+        })
+        .then(this.checkStatus)
+        .then(this.parseJSON)
+        .then(cb);
+    }
+    
+    checkStatus(response) {
+        if (response.status >= 200 && response.status < 300) {
+            return response;
+        }
+        const error = new Error(`HTTP Error ${response.statusText}`);
+        error.status = response.statusText;
+        error.response = response;
+        console.log(error); // eslint-disable-line no-console
+        throw error;
+    }
+    
+    parseJSON(response) {
+        return response.json();
+    }
+    
 
+    componentDidMount() {
+        this.setDataByDate(0, 99994323, (res) => {
+            for(var i=0;i<res.results.length;++i)
+                res.results[i][2] += 30;
+            this.mainSkymap.setData(res.results);
+            this.miniSkymap.setData(res.results);
+        });
+    }
+    
     render() {
         let setters = {
             setEcliptic: this.setEcliptic,
@@ -71,26 +101,26 @@ class Survey extends Component {
         return (
             <div className="survey-container">
                 <div>
-                     <h2>
+                    <h2>
                         SURVEY PROGRESS MONITOR
-                    </h2> 
-                     <button className="settings-button" type="button" onClick={this.setSidebar} aria-label="Settings">
+                    </h2>
+                    <button className="settings-button" type="button" onClick={this.setSidebar} aria-label="Settings">
                         <i className="fa fa-cog" aria-hidden="true"></i>
-                    </button> 
+                    </button>
                 </div>
                 <div className="main-container">
                     <div className="left-container">
-                         <SurveyControls/> 
-                         {/* <Charts/> */}
-                         <div className="main-skymap-wrapper">
-                            <MainSkymap ref={instance => { this.mainSkymap = instance; }} /> 
-                         </div>
+                        <SurveyControls />
+                        <Charts/>
+                        <div className="main-skymap-wrapper">
+                            <MainSkymap ref={instance => { this.mainSkymap = instance; }} />
+                        </div>
                     </div>
-                    <div className="right-container">                
-                        <MiniSkymaps ref={instance => { this.children.push(instance); }} onMinimapClick={this.setDisplayedFilter}/> 
+                    <div className="right-container">
+                        <MiniSkymaps ref={instance => { this.miniSkymap = instance; }} onMinimapClick={this.setDisplayedFilter} />
                     </div>
                 </div>
-                <Sidebar ref={instance => { this.sidebar = instance; }} {...setters} skymap={this.mainSkymap} /> 
+                <Sidebar ref={instance => { this.sidebar = instance; }} {...setters} skymap={this.mainSkymap} />
             </div>
         );
     }
