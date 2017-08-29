854dba1161e03d6c2198e0e94a84af2de6015df8
spereirag <sebastian.pereira@inria.cl>
2017-08-29 16:16:28 -0300

    [Backend] Integrate ts_sal Subscription to backend server. Emit incoming messages into websocket.

---
+++ LSST-server/emitters/ObservationEmitter.py
@@ -0,0 +1,68 @@
+import sys
+import time
+import datetime
+
+from SALPY_scheduler import *
+from flask_socketio import send, emit
+from flask_socketio import SocketIO
+
+#Get data from ts_sal connection
+def start_listening(app, socketio):
+    sal = SAL_scheduler()
+    sal.setDebugLevel(0)
+
+    topicTime           = scheduler_timeHandlerC()
+    topicObservation    = scheduler_observationC()
+    topicTarget         = scheduler_targetC()
+
+    sal.salTelemetrySub("scheduler_timeHandler")
+    sal.salTelemetrySub("scheduler_observation")
+    sal.salTelemetryPub("scheduler_target")
+
+    measInterval = 1
+
+    measCount  = 0
+    visitCount = 0
+    syncCount  = 0
+    targetId   = 0
+
+    stime = time.time()
+    print("SAL listening")
+    try:
+        while True:
+            time.sleep(0.110)
+            scode = sal.getNextSample_timeHandler(topicTime)
+            if scode == 0 and topicTime.timestamp != 0:
+
+                targetId += 1
+
+                topicTarget.targetId = targetId
+                topicTarget.fieldId  = 1234
+                topicTarget.filter   = "z"
+                topicTarget.ra       = 10.0
+                topicTarget.dec      = 30.0
+                topicTarget.angle    = 45.0
+                topicTarget.num_exposures = 2
+                sal.putSample_target(topicTarget)
+
+                while True:
+                    scode = sal.getNextSample_observation(topicObservation)
+                    if scode == 0 and topicObservation.targetId != 0:
+                        publish(app, socketio, topicObservation)
+                        measCount += 1
+                        visitCount += 1
+                        if topicTarget.targetId == topicObservation.targetId:
+                            syncCount += 1
+                            break
+                        else:
+                            print("UNSYNC targetId=%i observationId=%i" % (topicTarget.targetId, topicObservation.targetId))
+
+    except KeyboardInterrupt:
+        sal.salShutdown()
+        sys.exit(0)
+    
+# Publish data to WS connection
+def publish(app, socketio, topicObservation):
+    print('Emitting', [topicObservation.filter, topicObservation.ra, topicObservation.dec, 1])
+    with app.test_request_context('/'):
+        socketio.emit('data', [topicObservation.targetId, topicObservation.ra, topicObservation.dec, topicObservation.filter, 1])
--- LSST-server/server.py
+++ LSST-server/server.py
@@ -9,12 +9,28 @@ from utils import serialize
 import pandas as pd
 
 from routes import app
+from emitters.ObservationEmitter import start_listening
+from flask import Flask
+from flask_socketio import SocketIO
+from flask_socketio import send, emit
+import eventlet
+eventlet.monkey_patch()
 
-
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app, async_mode='eventlet')
 
 @app.route('/')
 def api_root():
-    return 'Welcome'
+    return '''
+<head>
+<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/socket.io/1.3.6/socket.io.min.js"></script>
+<script>
+    var socket = io();
+</script>
+</head>
+<body>Welcome</body>'''
 
 if __name__ == '__main__':
-    app.run()
+    eventlet.spawn(start_listening, app, socketio)
+    socketio.run(app)
+    send('message')
