895b6d96f3a0915838a48d51a4960c5254977737
spereirag <sebastian.pereira@inria.cl>
2017-10-06 17:42:15 -0300

    More style changes

--- LSST-app/src/components/SurveyControls/ModeSelection/ModeSelection.js
+++ LSST-app/src/components/SurveyControls/ModeSelection/ModeSelection.js
@@ -7,15 +7,15 @@ class ModeSelection extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            playbackModeActive: false,
+            playbackModeActive: true,
         }
     }
 
     handleChangeChk = () => {
         if(this.state.playbackModeActive)
-            this.props.setPlaybackMode();
-        else
             this.props.setLiveMode();        
+        else
+            this.props.setPlaybackMode();
         this.setState({
             playbackModeActive: !this.state.playbackModeActive,
         })
@@ -25,14 +25,14 @@ class ModeSelection extends Component {
         return (
             <div className="mode-selection">
                 <div>
-                    <h6>MODE SELECTION</h6>
+                    <h6>Mode selection</h6>
                 </div>
-                <span>Playback</span>
+                <span className={this.state.playbackModeActive ? 'highlight-text' : ''}>Playback</span>
                 <label className="switch">
-                    <input type="checkbox" checked={this.state.playbackModeActive} onChange={this.handleChangeChk}/>
+                    <input type="checkbox" checked={!this.state.playbackModeActive} onChange={this.handleChangeChk}/>
                     <span className="slider round"></span>
                 </label>
-                <span>Live</span>
+                <span className={!this.state.playbackModeActive ? 'highlight-text' : ''}>Live</span>
             </div>
         );
     }
