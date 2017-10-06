3859c482607dafa89e88a9776f3c1d6c65929bff
spereirag <sebastian.pereira@inria.cl>
2017-10-06 17:52:17 -0300

    Mode selectable by clicking mode name

--- LSST-app/src/components/SurveyControls/ModeSelection/ModeSelection.js
+++ LSST-app/src/components/SurveyControls/ModeSelection/ModeSelection.js
@@ -21,18 +21,32 @@ class ModeSelection extends Component {
         })
     }
 
+    setPlaybackMode = () => {
+        this.props.setPlaybackMode();
+        this.setState({
+            playbackModeActive: true,
+        })
+    }
+
+    setLiveMode = () => {
+        this.props.setLiveMode();
+        this.setState({
+            playbackModeActive: false,
+        })
+    }
+
     render() {
         return (
             <div className="mode-selection">
                 <div>
                     <h6>Mode selection</h6>
                 </div>
-                <span className={this.state.playbackModeActive ? 'highlight-text' : ''}>Playback</span>
+                <span className={this.state.playbackModeActive ? 'highlight-text' : ''} onClick={this.setPlaybackMode} >Playback</span>
                 <label className="switch">
                     <input type="checkbox" checked={!this.state.playbackModeActive} onChange={this.handleChangeChk}/>
                     <span className="slider round"></span>
                 </label>
-                <span className={!this.state.playbackModeActive ? 'highlight-text' : ''}>Live</span>
+                <span className={!this.state.playbackModeActive ? 'highlight-text' : ''} onClick={this.setLiveMode} >Live</span>
             </div>
         );
     }
