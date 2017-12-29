368795da4e9b8e3dac7a22554c2c1a64ab4c6823
Florencia <florencia@Constanzas-MacBook-Pro.local>
2017-12-29 11:58:31 -0300

    Revert "Se agrega un test para environment."
    This reverts commit 719b1e228f1be4c758f78eddcd9ac8cd89766a6e.

--- LSST-app/src/components/Environment/environment.test.js
+++
@@ -1,9 +0,0 @@
-/*import React from 'react';
-import Environment from '../Environment';
-import renderer from 'react-test-renderer';
-it('renders correctly', () => {
-  const tree = renderer
-    .create(<Environment>PRUEBAAAA</Environment>)
-    .toJSON();
-  expect(tree).toMatchSnapshot();
-});*/
