b856d85e6da8ea12e9d1956b13043e6a6e0dd4c7
Florencia <flo.mirandai@gmail.com>
2018-01-03 12:56:45 -0300

    Environment test mas lindo.

--- LSST-app/src/components/Environment/environment.test.js
+++ LSST-app/src/components/Environment/environment.test.js
@@ -2,11 +2,17 @@ import React from 'react';
 import Environment from './Environment';
 import renderer from 'react-test-renderer';
 
+/*----Snapshot testing----*/
+describe('environment_Test',function(){
+  it('renders correctly', () => {
+    const tree = renderer
+      .create(<Environment />)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+  
+})
 
-it('renders correctly', () => {
-  const tree = renderer
-    .create(<Environment />)
-    .toJSON();
-  expect(tree).toMatchSnapshot();
-});
+
+/*----Other test ----*/
 
