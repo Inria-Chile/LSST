afed81393791fef10d8524ad8d5c724e64ba8519
Florencia <flo.mirandai@gmail.com>
2018-01-04 18:00:15 -0300

    Test de DomeAzimuthTimeSeries falla por Date()

--- LSST-app/src/components/Dome/TimeSeries/DomeAzimuthTimeSeries/DomeAzimuthTimeSeries.test.js
+++ LSST-app/src/components/Dome/TimeSeries/DomeAzimuthTimeSeries/DomeAzimuthTimeSeries.test.js
@@ -10,10 +10,11 @@ describe('DomeAzimuthTimeSeries_Test', function(){
     const renderer = new shallowRenderer();
     const tree = renderer.render(
 
+    //ASK: Revisar si los numeros son realistas. 
     <DomeAzimuthTimeSeries domeAzimuth={30}
-    domeTargetAzimuth={75}
+    domeTargetAzimuth={10}
     domeOptimalAzimuth={40}
-    timestamp={new Date()}
+    timestamp={"2018-01-04T19:44:10.611Z"}
     width={600} height={350} />
     );
     expect(tree).toMatchSnapshot();
