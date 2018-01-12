0a4a73d6d8729e97d5c95811c2a6ba0d9b1749d9
Florencia <flo.mirandai@gmail.com>
2018-01-12 17:52:04 -0300

    tests for DomeAzimuthTimeSeries added

--- LSST-app/src/components/Dome/TimeSeries/DomeAzimuthTimeSeries/__tests__/DomeAzimuthTimeSeries_snaphsot.test.js
+++ LSST-app/src/components/Dome/TimeSeries/DomeAzimuthTimeSeries/__tests__/DomeAzimuthTimeSeries_snaphsot.test.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import DomeAzimuthTimeSeries from './DomeAzimuthTimeSeries';
+import DomeAzimuthTimeSeries from '../DomeAzimuthTimeSeries';
 import renderer from 'react-test-renderer';
 import shallowRenderer from 'react-test-renderer/shallow';
 
 /*---- Snapshot testing ----*/
 describe('DomeAzimuthTimeSeries_Test', function(){
 
-  it('renders correctly', () => {
+  it('renders correctly when timestamp is setted', () => {
     const renderer = new shallowRenderer();
     const mockedDate = new Date("2018-01-04T19:44:10.611Z");
     const originalDate = Date;
@@ -18,6 +18,23 @@ describe('DomeAzimuthTimeSeries_Test', function(){
     domeTargetAzimuth={0}
     domeOptimalAzimuth={0}
     timestamp={"2018-01-04T19:44:10.611Z"}
+    width={600} height={350} />
+    );
+    expect(tree).toMatchSnapshot();
+  });
+
+
+  it('renders correctly when timestamp is not setted', () => {
+    const renderer = new shallowRenderer();
+    const mockedDate = new Date("2018-01-04T19:44:10.611Z");
+    const originalDate = Date;
+    global.Date = jest.fn(() => mockedDate);
+    global.Date.getTime = originalDate.getTime;
+
+    const tree = renderer.render(
+    <DomeAzimuthTimeSeries domeAzimuth={0}
+    domeTargetAzimuth={0}
+    domeOptimalAzimuth={0}
     width={600} height={350} />
     );
     expect(tree).toMatchSnapshot();
