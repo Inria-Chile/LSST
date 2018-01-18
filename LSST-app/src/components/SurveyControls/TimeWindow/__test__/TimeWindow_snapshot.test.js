6109f9d534e49f5882a01cb12f6eafbd0d5eda83
Florencia <flo.mirandai@gmail.com>
2018-01-18 17:44:53 -0300

    TimeWindow test added

--- LSST-app/src/components/SurveyControls/TimeWindow/__test__/TimeWindow_snapshot.test.js
+++ LSST-app/src/components/SurveyControls/TimeWindow/__test__/TimeWindow_snapshot.test.js
@@ -1,20 +1,15 @@
 import React from 'react';
-import TimeWindow from './TimeWindow';
+import TimeWindow from '../TimeWindow';
 import renderer from 'react-test-renderer';
-import Survey from '../../Survey/Survey';
-
-
 
 /*---- Snapshot testing ----*/
 describe('TimeWindow_Test',function(){
 
     it('renders correctly without shallowRender', () => {
-        const tree = renderer
-          .create(<TimeWindow setTimeWindow={Survey.setTimeWindow} />)
+      let setTimeWindow = jest.fn();
+      const tree = renderer
+          .create(<TimeWindow setTimeWindow={setTimeWindow} />)
           .toJSON();
         expect(tree).toMatchSnapshot();
       });
-
-
-})
-/*---- Other tests ----*/
+});
