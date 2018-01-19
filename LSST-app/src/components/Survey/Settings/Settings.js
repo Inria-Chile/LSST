2d564c89eb10d34fa00ea98adc5799c17be8bfc2
Florencia <flo.mirandai@gmail.com>
2018-01-19 16:17:09 -0300

    Settings snapshot updated, sidebar name changed from spymap to Skymap

--- LSST-app/src/components/Survey/Settings/Settings.js
+++ LSST-app/src/components/Survey/Settings/Settings.js
@@ -79,7 +79,7 @@ class Settings extends Component {
                 <button className="settings-button" type="button" onClick={this.toggleSidebar} aria-label="Settings">
                     <i className="fa fa-cog" aria-hidden="true"></i>
                 </button>
-                <div className={"sidebar " + (this.state.sidebarOpen ? 'sidebar-open' : '')}>skymap
+                <div className={"sidebar " + (this.state.sidebarOpen ? 'sidebar-open' : '')}>Skymap
                     <div className="checkbox">
                         <label className='selectable'>
                             <input type="checkbox" value="" onChange={this.toggleEcliptic} defaultChecked={this.state.showEcliptic}/> Ecliptic plane
