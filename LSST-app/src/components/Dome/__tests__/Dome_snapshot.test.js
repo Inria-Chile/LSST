e04e546d4f394430d891a1e31458630663f61143
Florencia <flo.mirandai@gmail.com>
2018-01-25 16:53:21 -0300

    Dome snapshot test refactor

--- LSST-app/src/components/Dome/__tests__/Dome_snapshot.test.js
+++ LSST-app/src/components/Dome/__tests__/Dome_snapshot.test.js
@@ -1,22 +1,50 @@
 import React from 'react';
 import Dome from '../Dome';
-import renderer from 'react-test-renderer';
-import shallowRenderer from 'react-test-renderer/shallow';
-
-
-
-/**
- * Test funciona pero muestra el log en la consola. 
- */
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import {shallow} from "enzyme"; 
+import toJson from 'enzyme-to-json';
+import SocketMock from 'socket.io-mock'
+Enzyme.configure({ adapter: new Adapter() });
 
 
 /*---- Snapshot testing ----*/
 describe('Dome test', function(){
-
-    it('renders correctly with shallowRender',()=>{
-        const renderer = new shallowRenderer();
-        const tree = renderer.render(
+    let socket,message,dome;
+    beforeEach(()=>{
+        dome = shallow(
             <Dome/>);
-            expect(tree).toMatchSnapshot(); 
+        let msg = {
+            domeAzimuth: 0,
+            telescopeElevation: 0,
+            domeTargetAzimuth: 0,
+            telescopeTargetElevation:0,
+            mountAzimuth: 0,
+            timestamp: "2018-01-04T19:44:10.611Z",
+        }
+        socket = new SocketMock();
+        socket.on('DomePosition',msg=>{
+            dome.setState({
+                domeAzimuth: msg.DomeAzPos,
+                telescopeElevation: msg.DomeElPos,
+                domeTargetAzimuth: msg.DomeAzCMD,
+                telescopeTargetElevation: msg.DomeElCMD,
+                mountAzimuth: msg.DomeAzPos,
+                timestamp: msg.timestamp,
+            })
+        });
+        message={
+            DomeAzPos : 0,
+            DomeElPos : 0,
+            DomeAzCMD : 0,
+            DomeElCMD : 0,
+            DomeAzPos : 0,
+            timestamp : "2018-01-04T19:44:10.611Z",
+        }
+        socket.socketClient.emit('DomePosition',message);
+    });
+    it('renders correctly with shallowRender',()=>{
+
+        expect(toJson(dome)).toMatchSnapshot(); 
     })
 })
