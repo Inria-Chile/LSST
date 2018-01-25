eedb12097e693fbbbda3720145be70bd7fecba5e
Florencia <flo.mirandai@gmail.com>
2018-01-25 17:11:20 -0300

    DomeAzimuthTimeSeries snapshot test refactor

--- LSST-app/src/components/Dome/TimeSeries/DomeAzimuthTimeSeries/__tests__/DomeAzimuthTimeSeries_snaphsot.test.js
+++ LSST-app/src/components/Dome/TimeSeries/DomeAzimuthTimeSeries/__tests__/DomeAzimuthTimeSeries_snaphsot.test.js
@@ -5,37 +5,33 @@ import shallowRenderer from 'react-test-renderer/shallow';
 
 /*---- Snapshot testing ----*/
 describe('DomeAzimuthTimeSeries_Test', function(){
+  
+  /*setup*/
+  const renderer = new shallowRenderer();
+  /* mock date */
+  const mockedDate = new Date("2018-01-04T19:44:10.611Z");
+  const originalDate = Date;
+  global.Date = jest.fn(() => mockedDate);
+    
 
-  it('renders correctly when timestamp is setted', () => {
-    const renderer = new shallowRenderer();
-    const mockedDate = new Date("2018-01-04T19:44:10.611Z");
-    const originalDate = Date;
-    global.Date = jest.fn(() => mockedDate);
-    global.Date.getTime = originalDate.getTime;
-
+  it('renders correctly when timestamp is setted', () => {   
     const tree = renderer.render(
-    <DomeAzimuthTimeSeries domeAzimuth={0}
-    domeTargetAzimuth={0}
-    domeOptimalAzimuth={0}
-    timestamp={"2018-01-04T19:44:10.611Z"}
-    width={600} height={350} />
+      <DomeAzimuthTimeSeries domeAzimuth={0}
+      domeTargetAzimuth={0}
+      domeOptimalAzimuth={0}
+      timestamp={"2018-01-04T19:44:10.611Z"}
+      width={600} height={350} />
     );
     expect(tree).toMatchSnapshot();
   });
 
 
   it('renders correctly when timestamp is not setted', () => {
-    const renderer = new shallowRenderer();
-    const mockedDate = new Date("2018-01-04T19:44:10.611Z");
-    const originalDate = Date;
-    global.Date = jest.fn(() => mockedDate);
-    global.Date.getTime = originalDate.getTime;
-
     const tree = renderer.render(
-    <DomeAzimuthTimeSeries domeAzimuth={0}
-    domeTargetAzimuth={0}
-    domeOptimalAzimuth={0}
-    width={600} height={350} />
+      <DomeAzimuthTimeSeries domeAzimuth={0}
+      domeTargetAzimuth={0}
+      domeOptimalAzimuth={0}
+      width={600} height={350} />
     );
     expect(tree).toMatchSnapshot();
   });
