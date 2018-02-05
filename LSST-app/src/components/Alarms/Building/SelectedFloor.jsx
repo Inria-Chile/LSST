f62b11838deeede85c1b06246f8dfcc273cce5ae
Jose Galaz <jdgalaz@uc.cl>
2018-02-05 12:08:24 -0300

    add many alarms on SelectedFloor and print coordinate on click, #20

--- LSST-app/src/components/Alarms/Building/SelectedFloor.jsx
+++ LSST-app/src/components/Alarms/Building/SelectedFloor.jsx
@@ -3,6 +3,29 @@ import React, { Component } from 'react';
 import './SelectedFloor.css';
 
 class SelectedFloor extends Component {
+    constructor(props){
+        super(props);
+        this.alarmsCoordinates = [[3003-441,524-55],
+        [3002-441,770-55],
+        [3206-441,209-55],
+        [3270-441,208-55],
+        [3248-441,276-55],
+        [3248-441,373-55],
+        [3248-441,534-55],
+        [3245-441,700-55],
+        [3248-441,796-55],
+        [3372-441,198-55],
+        [3439-441,154-55],
+        [3502-441,373-55],
+        [3502-441,532-55],
+        [3506-441,702-55],
+        [3502-441,795-55],
+        [3697-441,200-55],
+        [3699-441,436-55],
+        [3697-441,557-55],
+        [3698-441,655-55],
+        [3697-441,845-55]];
+    }
 
     static floorPlanImages = {
         floor1: '/img/floor_plans/floor1.png',
@@ -15,6 +38,8 @@ class SelectedFloor extends Component {
         floor8: '/img/floor_plans/floor8.png',
     }
 
+
+
     // constructor(props) {
     //     super(props);
     // }
@@ -25,31 +50,42 @@ class SelectedFloor extends Component {
             imgSrc = "";
 
 
-
+        const h = 16 ;
+        const w = 16 ;
         return (
 
             <div className={["selected-floor-container", this.props.floorAlarms.indexOf(this.props.selectedFloor) > -1 ? 'alarm' : ''].join(' ')}>
-                <div  height="100%" width="100%" style={{position:"relative",opacity:"70%"}}>
-                    
-                    <Alarm position= {[(2251.5)/2989*100, (403)/1369*100]}/>
-                <img src={imgSrc} className="selected-floor-plan" alt="selected floor" />
-                {/* <Alarm/> */}
+                <div height="100%" width="100%" style={{ position: "relative", opacity: "70%" }}>
+
+                    {this.alarmsCoordinates.map(alarmCoordinate => {
+                        return(                    
+                            <Alarm
+                            position={[(alarmCoordinate[0]  -0.5*w) / 3391 * 100, (alarmCoordinate[1] -0.5*h) / 1555.86 * 100]}
+                            width={w}
+                            height={h} />
+                        );
+                    })}
+
+                    <img src={imgSrc} className="selected-floor-plan" alt="selected floor" onClick={(e) => {
+                        console.log(e.clientX,e.clientY);
+                    }} />
+
                 </div>
             </div>
         );
     }
-}                
-class Alarm extends Component{
-    render(){
-            
-        return(
-            <img src="/img/floor_plans/fire_alarm-on.svg" 
-                    style={{
-                        left:this.props.position[0]+"%",
-                        top:this.props.position[1]+"%",
-                        position:'absolute',
-                        width:"60px", 
-                        height:"40px"}}/>
+}
+class Alarm extends Component {
+    render() {
+        return (
+            <img src="/img/floor_plans/fire_alarm-on.svg"
+                style={{
+                    left: this.props.position[0] + "%",
+                    top: this.props.position[1] + "%",
+                    position: 'absolute',
+                    width: this.props.width + "px",
+                    height: this.props.height + "px"
+                }} />
         );
     }
 }
