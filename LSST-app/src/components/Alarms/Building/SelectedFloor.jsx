6cd3f8697f2fdd59dc5df5d555f265ac7cb0fd24
Jose Galaz <jdgalaz@uc.cl>
2018-02-05 12:22:50 -0300

    move Alarm component to separate file

--- LSST-app/src/components/Alarms/Building/SelectedFloor.jsx
+++ LSST-app/src/components/Alarms/Building/SelectedFloor.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 // import ReactDOM from 'react-dom';
 import './SelectedFloor.css';
+import Alarm from './Alarm.jsx';
 
 class SelectedFloor extends Component {
     constructor(props){
@@ -76,36 +77,6 @@ class SelectedFloor extends Component {
         );
     }
 }
-class Alarm extends Component {
-    constructor(props){
-        super(props);
-        this.images =[
-            "/img/floor_plans/fire_alarm-on.svg",
-            "/img/floor_plans/fire_alarm-off.svg"
-        ];
 
-        this.state = {
-            isOn: true
-        };
-    }
-    render() {
-        return (
-            <img alt="alarm icon"
-                src= {this.state.isOn? this.images[0]: this.images[1]}
-                style={{
-                    left: this.props.position[0] + "%",
-                    top: this.props.position[1] + "%",
-                    position: 'absolute',
-                    width: this.props.width + "px",
-                    height: this.props.height + "px"
-                }} 
-                onClick = {(e) =>{
-                    this.setState({
-                        isOn: !this.state.isOn
-                    });
-                }}/>
-        );
-    }
-}
 
 export default SelectedFloor;
