292f9e679d0512b1a6088d79982d57612b269376
Jose Galaz <jdgalaz@uc.cl>
2018-02-12 16:02:37 -0300

    align alarms in floor svg with no margin

--- LSST-app/src/components/Alarms/Building/SelectedFloor.jsx
+++ LSST-app/src/components/Alarms/Building/SelectedFloor.jsx
@@ -6,34 +6,36 @@ import Alarm from './Alarm.jsx';
 class SelectedFloor extends Component {
     constructor(props){
         super(props);
-        this.alarmsCoordinates = [[3003-441,524-55],
-        [3002-441,770-55],
-        [3206-441,209-55],
-        [3270-441,208-55],
-        [3248-441,276-55],
-        [3248-441,373-55],
-        [3248-441,534-55],
-        [3245-441,700-55],
-        [3248-441,796-55],
-        [3372-441,198-55],
-        [3439-441,154-55],
-        [3502-441,373-55],
-        [3502-441,532-55],
-        [3506-441,702-55],
-        [3502-441,795-55],
-        [3697-441,200-55],
-        [3699-441,436-55],
-        [3697-441,557-55],
-        [3698-441,655-55],
-        [3697-441,845-55]];
+        const offsetX = 433+7;
+        const offsetY = 52+4;
+        this.alarmsCoordinates = [[2954-offsetX, 520-offsetY],
+        [2952-offsetX, 761-offsetY],
+        [3154-offsetX, 207-offsetY],
+        [3216-offsetX, 206-offsetY],
+        [3193-offsetX, 274-offsetY],
+        [3193-offsetX, 368-offsetY],
+        [3192-offsetX, 525-offsetY],
+        [3193-offsetX, 690-offsetY],
+        [3193-offsetX, 782-offsetY],
+        [3315-offsetX, 196-offsetY],
+        [3380-offsetX, 151-offsetY],
+        [3443-offsetX, 368-offsetY],
+        [3444-offsetX, 527-offsetY],
+        [3443-offsetX, 691-offsetY],
+        [3444-offsetX, 781-offsetY],
+        [3637-offsetX, 198-offsetY],
+        [3636-offsetX, 432-offsetY],
+        [3634-offsetX, 546-offsetY],
+        [3635-offsetX, 645-offsetY],
+        [3635-offsetX, 833-offsetY]];
     }
 
     static floorPlanImages = {
-        floor1: '/img/floor_plans/floor1.png',
-        floor2: '/img/floor_plans/floor1_noalarms.svg',
+        floor1: '/img/floor_plans/n2-floor_plan.svg',
+        floor2: '/img/floor_plans/n2-floor_plan-noalarms.svg',
         // floor2: '/img/floor_plans/floor2.png',
-        floor3: '/img/floor_plans/floor3.png',
-        floor4: '/img/floor_plans/floor4.png',
+        floor3: '/img/floor_plans/floor1.png',
+        floor4: '/img/floor_plans/floor1_noalarms.svg',
         floor5: '/img/floor_plans/floor5.png',
         floor6: '/img/floor_plans/floor6.png',
         floor7: '/img/floor_plans/floor7.png',
@@ -52,8 +54,8 @@ class SelectedFloor extends Component {
             imgSrc = "";
 
 
+        const w = 25;
         const h = 25;
-        const w = 25 ;
         return (
 
             <div className={["selected-floor-container", this.props.floorAlarms.indexOf(this.props.selectedFloor) > -1 ? 'alarm' : ''].join(' ')}>
@@ -63,7 +65,7 @@ class SelectedFloor extends Component {
                         return(                    
                             <Alarm
                             key = {index.toString()}
-                            position={[(alarmCoordinate[0]  -0.5*w) / 3391 * 100, (alarmCoordinate[1] -0.5*h) / 1555.86 * 100]}
+                            position={[(alarmCoordinate[0]  - 0.5*w)/ 3333 * 100, (alarmCoordinate[1]-0.5*h)/ 1530  * 100]}
                             width={w}
                             height={h} />
                         );
