951eaaa0b09e19c31cefa9de9641b5219df11956
Jose Galaz <jdgalaz@uc.cl>
2018-02-13 10:22:20 -0300

    take "F" icons off from n1-floor and re-align

--- LSST-app/src/components/Alarms/Building/SelectedFloor.jsx
+++ LSST-app/src/components/Alarms/Building/SelectedFloor.jsx
@@ -14,15 +14,15 @@ class SelectedFloor extends Component {
 
         static floorPlans = {
         floor1: { 
-            url: '/img/floor_plans/n1-floor_plan-text.svg', 
-            alarmsCoordinates: [[2955,247],[2973,878],[3169,245],[3169,526],[3170,879],[3215,114],[3367,242],[3422,194],[3407,564],[3388,878],[3663,295],[3663,567],[3659,860]]
+            url: '/img/floor_plans/n1-floor_plan-text-noalarms.svg', 
+            alarmsCoordinates: [[2955,247],[2973,863],[3169,245],[3169,526],[3170,863],[3215,114],[3367,242],[3422,194],[3407,564],[3388,863],[3663,295],[3663,567],[3659,863]]
         },
         floor2: {
             url:'/img/floor_plans/n2-floor_plan.svg' ,
             alarmsCoordinates: [[2947,598],[2943,841],[3143,288],[3208,288],[3183,353],[3184,447],[3182,605],[3180,769],[3182,862],[3306,278],[3370,232],[3433,448],[3433,605],[3435,770],[3433,862],[3624,281],[3621,507],[3624,627],[3624,724],[3624,912]]   
         },
-        floor3:{ url: '/img/floor_plans/floor5.png', alarmsCoordinates: []},
-        floor4:{ url: '/img/floor_plans/floor5.png', alarmsCoordinates: []},
+        floor3:{ url: '/img/floor_plans/floor3.png', alarmsCoordinates: []},
+        floor4:{ url: '/img/floor_plans/floor4.png', alarmsCoordinates: []},
         floor5:{ url: '/img/floor_plans/floor5.png', alarmsCoordinates: []},
         floor6:{ url: '/img/floor_plans/floor6.png', alarmsCoordinates: []},
         floor7:{ url: '/img/floor_plans/floor7.png', alarmsCoordinates: []},
@@ -43,7 +43,6 @@ class SelectedFloor extends Component {
             return [(coord[0]-this.offsetX)/3333*100, (coord[1]-this.offsetY)/1611*100];
         });
         
-;
 
         return (
 
